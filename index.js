c4dd3225a23f1f6f7524f67596a0d1a10b184774
AMKlein2018 <adamklein0519@gmail.com>
2018-04-25 23:22:52 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Code Challenge 1
+
+// var button1 = document.getElementById("myBtn")
+// var button2 = document.getElementById("myBtn2")
+
+
+// document.getElementById("myBtn").addEventListener("click", function(){
+// 	document.getElementById("demo").innerHTML = "I'm right";
+// })
+
+// document.getElementById("myBtn2").addEventListener("click", function(){
+// 	document.getElementById("demo2").innerHTML = "I'm right";
+// })
+
+
+// Code Challenge 2
+
+// function myOverFunction () {
+// 	alert("Hey, I told you not to hover over me");
+// }
+
+
+
+// Code Challenge 3
+
+function myFunction() {
+    alert ("Hi");
+}
+
